f6cddf49026964165383d714378c63d83b7dd3bd
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-02-15 12:11:46 +0000

    feat: setup app, env, pool & router templates

---
+++ app.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+const router = require("./routes/router");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+app.use("/", router);
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Express app listening on port ${PORT}!`);
+});
---
+++ routes/router.js
@@ -0,0 +1,5 @@
+const { Router } = require("express");
+const router = Router();
+const controller = require("../controllers/controller");
+
+module.exports = router;
