519507667b1afa67ecf7ed1c6a2f0ba9acc66b3b
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-02-15 12:45:22 +0000

    feat: add all paths in router

--- routes/router.js
+++ routes/router.js
@@ -2,4 +2,29 @@ const { Router } = require("express");
 const router = Router();
 const controller = require("../controllers/controller");
 
+// read all systems
+router.get("/");
+
+// create new system form
+router.get("/new-system");
+router.post("/new-system");
+
+// read system and all games
+router.get("/:system-name");
+
+// update form, delete system.
+router.get("/:system-name/update");
+router.post("/:system-name/update");
+
+// create new game form
+router.get("/:system-name/new-game");
+router.post("/:system-name/new-game");
+
+//read game
+router.get("/:gameId");
+
+// update form, delete game.
+router.get("/:gameId/update");
+router.post("/:gameId/update");
+
 module.exports = router;
